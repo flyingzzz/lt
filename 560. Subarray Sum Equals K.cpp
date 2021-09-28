093dad3ea383429f0c81cb61c3d3f37648ba632b
flyingzzz <91492735+flyingzzz@users.noreply.github.com>
2021-09-27 19:16:54 -0700

    Create 560. Subarray Sum Equals K.cpp

---
+++ 560. Subarray Sum Equals K.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> prefix_sum;
+        prefix_sum[0] = 1;
+        
+        int sum = 0;
+        int ans = 0;
+        for(int num : nums) {
+            sum += num;
+            int target = sum - k;
+            ans += prefix_sum[target];
+            prefix_sum[sum]++;
+        }
+        
+        return ans;
+    }
+};
